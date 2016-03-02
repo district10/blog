40a1a8d9badbb78c0af449bcbb7305695a7b50c2
Tang Zhixiong <dvorak4tzx@gmail.com>
2016-03-02 08:44:11 +0800

    lazy commit

---
+++ _statics/blog-query.js
@@ -0,0 +1,19 @@
+// works like a url shortener
+var tzx_goto = {
+    "got": 'http://tangzx.qiniudn.com/post-0086-game-of-throne-series.html',
+}
+
+// query and go
+var tzx_link_prefix = 'http://tangzx.qiniudn.com';
+var tzx_links = [
+    {
+        url: '/post-0086-game-of-throne-series.html',
+        title: 'Game of Throne',
+        query: ['gof', 'game of throne']
+    },
+    { 
+        url: '/post-0079-lingoes-dict.html',
+        title: 'Lingoes 网络词典',
+        query: ['lingoes', 'dict']
+    },
+];
