3ac7d3d958fa82034a3b5bdaac593d21305f176c
Tang Zhixiong <dvorak4tzx@gmail.com>
2016-03-22 14:45:16 +0800

    2016年03月22日  6:45:16 - MINGW64_NT-6.1

--- filter.pl
+++ filter.pl
@@ -6,6 +6,7 @@ my @mods = `git diff-tree --no-commit-id --name-only -r HEAD`;
 push @mods, 'footer.html';
 push @mods, 'filter.pl';
 push @mods, 'Makefile';
+print 'files modified: @mods.\n';
 
 my $hits = join '', @mods;
 my @all_pubs = <*>;
--- publish.mk
+++ publish.mk
@@ -3,7 +3,7 @@ HTML = $(MD:%.md=%.html)
 
 PANDOC_OPTIONS = -S -s --ascii \
 	-c main.css \
-	-A ../footer.html \
+	-A footer.html \
 	--highlight-style pygments \
 	-f markdown+east_asian_line_breaks
 
