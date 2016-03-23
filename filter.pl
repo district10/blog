0cd1df371cd76dfabd70da62be98f20fec044718
Tang Zhixiong <dvorak4tzx@gmail.com>
2016-03-23 14:02:04 +0800

    2016年03月23日  6:02:04 - MINGW64_NT-6.1

--- filter.pl
+++ filter.pl
@@ -7,8 +7,8 @@ push @mods, "footer.html\n";
 push @mods, "filter.pl\n";
 push @mods, "Makefile\n";
 
-my $hits = join '', @mods;
-print "files to protect [\n", $hits, "]\n";
+my $hits = join "\t", @mods;
+print "files to protect [\n\t", $hits, "]\n";
 
 my @all_pubs = <*>;
 foreach (@all_pubs) {
