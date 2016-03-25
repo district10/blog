69c2087282290bed1d8ea9fc735e9d22377b06ef
Tang Zhixiong <gnat_tang@yeah.net>
2016-03-26 02:34:19 +0800

    干得漂亮！

--- publish.mk
+++ publish.mk
@@ -1,28 +1,34 @@
-MD   = $(wildcard *.md)
-HTML = $(MD:%.md=%.html)
-
++EAST_ASIAN_LINE_BREAKS=$(echo '' | pandoc -f markdown+east_asian_line_breaks 2&>/dev/null && echo -n '+east_asian_line_breaks' || echo -n '')
 PANDOC_OPTIONS = -S -s --ascii \
 	-c main.css \
 	-A footer.html \
 	--highlight-style pygments \
-	-f markdown+east_asian_line_breaks
+	-f markdown$(+EAST_ASIAN_LINE_BREAKS)
+
+MD   = $(wildcard *.md)
+HTML = $(MD:%.md=%.html)
 
 all: html
-
 html: $(HTML)
 
+post-0051-spatial-gis.html: post-0051-spatial-gis.bib
+post-0051-spatial-gis.bib: 
+	cp ../_posts/$@ $@
 post-0051-spatial-gis.html: post-0051-spatial-gis.md
 	pandoc \
 		$(PANDOC_OPTIONS) \
 		--toc --mathjax \
-		--bibliography $(patsubst %.md, %.bib, $^) \
+		--bibliography $(patsubst %.md, %.bib, $<) \
 		$< \
 		-o $@
+post-0050-gis-overall.html: post-0050-gis-overall.bib
+post-0050-gis-overall.bib:
+	cp ../_posts/$@ $@
 post-0050-gis-overall.html: post-0050-gis-overall.md
 	pandoc \
 		$(PANDOC_OPTIONS) \
 		--toc --mathjax \
-		--bibliography $(patsubst %.md, %.bib, $^) \
+		--bibliography $(patsubst %.md, %.bib, $<) \
 		$< \
 		-o $@
 
@@ -45,3 +51,6 @@ notes.html: notes.md
 		--toc --mathjax \
 		$< \
 		-o $@
+
+m:
+	$(EDITOR) Makefile
